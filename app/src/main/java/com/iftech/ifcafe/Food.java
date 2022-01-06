9983234edb1237ab86a74ccdaeddb83409284899
imamfirdaus-if <imamfirdaus3110@gmail.com>
2022-01-06 22:17:34 +0700

    upload version beta

---
+++ app/src/main/java/com/iftech/ifcafe/Food.java
@@ -0,0 +1,18 @@
+package com.iftech.ifcafe;
+
+import android.graphics.drawable.Drawable;
+
+public class Food {
+    String nama;
+    String deskripsi;
+    int harga;
+    Drawable image;
+
+    public Food(String nama, String deskripsi, int harga, Drawable image) {
+        this.nama = nama;
+        this.deskripsi = deskripsi;
+        this.harga = harga;
+        this.image = image;
+    }
+}
+
